e829f51475ffea1ff621a8cba26a4d3067c47c37
Deen <djgangen@gmail.com>
2020-12-14 23:12:28 +0200

    Added question.dart and creating a custom widget in it

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './question.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  build(BuildContext context) {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+    print('Answer Chosen');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var _questions = [
+      "What's your favourite colour?",
+      "What's your favourite animal?",
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App!'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Question(_questions[_questionIndex]),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('Answer 2 Chosen'),
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  print('Answer 3 chosen');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
